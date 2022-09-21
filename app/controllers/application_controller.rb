9cbcd46fab53d4814be811923fb39dd5cc7d6e00
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-21 18:09:57 +0900

    新規登録、ログイン、ログアウト実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   private
 
   def basic_auth
@@ -7,4 +9,11 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
     end
   end
-end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name,:last_name,:first_kana,:last_kana,:birthday])
+     
+     
+    end
+    
+  end
+
