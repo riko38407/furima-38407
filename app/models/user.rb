1f9ae85f265fb7231f352259f5de241916edf33e
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-21 10:30:49 +0900

    バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_kana, presence: true
+  validates :last_kana, presence: true
+  validates :birthday, presence: true
 end
