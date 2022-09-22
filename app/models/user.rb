e9b7bcdf13d91a3b57c91ec3f5db3bfa05dfd33c
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-22 11:29:05 +0900

    userモデルの単体テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  has_many :buying_historys
+         
   validates :nickname, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
