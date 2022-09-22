4441270f352edc39f5e25ff7c39fc90ff5da2872
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-22 17:54:14 +0900

    バリデーション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,3 @@ class ItemsController < ApplicationController
   def create
   end
 end
-
-private
-
-def move_to_index
-  redirect_to action: :index unless user_signed_in?
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :buying_historys
+  #has_many :items
+  #has_many :buying_historys
 
   with_options presence: true do
     
-    validates :password, format: { with: /\A[a-z0-9]+\z/i, message: 'Include both letters and numbers' }, allow_nil: true
+ VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+ validates :password, format: { with: VALID_PASSWORD_REGEX }
+
 
     validates :nickname, uniqueness: true
-    validates :email,    uniqueness: true
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    
+    validates :first_name, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
+    validates :last_name, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
     validates :first_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :last_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday, presence: true
