becb26a64f346a54b196634ed5f0ed86e8e5ec1d
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-23 20:51:21 +0900

    ActiveHash実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,27 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
+
 
   def index
+    @items = Item.includes(:user).order('created_at DESC')
+  end
+
+  def new
+    @item =  Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+private
+
+def item_params
+  params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,30 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
   has_one_attached :image
+
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :prefecture_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :shipping_date_id
+  end
 end
