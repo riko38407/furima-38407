a0e35c3244aa1a83ef46cdd95fafd263cf26c7c5
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-24 19:33:40 +0900

    Rubocop 実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -19,9 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
-private
+  private
 
-def item_params
-  params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
-end
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id,
+                                 :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
 end
