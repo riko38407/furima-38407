255df79e6098bbfe8403aab8b037450706c8c9ec
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-24 19:28:31 +0900

    テストコード修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :user
+  belongs_to :user 
   belongs_to :category
   belongs_to :prefecture
   belongs_to :item_status
