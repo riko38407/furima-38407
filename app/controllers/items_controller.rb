2eaaae88db6817fc7ce916b19a1cf5f1bdb20796
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-25 17:49:08 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,11 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    #@items = Item.includes(:user).order('created_at DESC')
+    # @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
-    @item =  Item.new
+    @item = Item.new
   end
 
   def create
--- app/models/item.rb
+++ app/models/item.rb
@@ -17,10 +17,10 @@ class Item < ApplicationRecord
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price, numericality:{ with: /\A[0-9]+\z/, message: 'should be half-width numbers'}
+    validates :price, numericality: { with: /\A[0-9]+\z/, message: 'should be half-width numbers' }
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
-  with_options numericality: { other_than: 0, message: 'Select'} do
+  with_options numericality: { other_than: 0, message: 'Select' } do
     validates :category_id
     validates :prefecture_id
     validates :item_status_id
@@ -28,4 +28,3 @@ class Item < ApplicationRecord
     validates :shipping_date_id
   end
 end
-
