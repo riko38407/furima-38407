b57673b80babbb6316ffc84e4ae12a7940042b28
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-28 11:10:21 +0900

    orderコントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,24 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @order = Order.new
+    @order_form = OrderForm.new
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
-      @order.save
-      return redirect_to root_path
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:price)
+   params.require(:order_form).permit(:prefecture_id, :city, :house_number, :building_name, :phone_number,:post_code).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
