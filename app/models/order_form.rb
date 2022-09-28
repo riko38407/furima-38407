f3037ac0abb32328c8dd311f7dbe6e8c40f34979
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-28 11:03:32 +0900

    order_formテーブル作成

---
+++ app/models/order_form.rb
@@ -0,0 +1,21 @@
+class OrderForm
+include ActiveModel::Model
+
+attr_accessor  :user_id, :item_id, :prefecture_id, :city, :house_number, :building_name, :phone_number, :post_code
+
+with_options presence: true do
+validates :user_id
+validates :item_id
+validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+validates :city
+validates :house_number
+validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+end
+
+def save
+  order = Order.create(user_id: user_id, item_id: item_id)
+  Payment.create(order_id: order.id, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, post_code: post_code)
+end
+
+end
