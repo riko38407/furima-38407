b0c7266f334960095a77d45e6f968cb757033799
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-28 16:30:14 +0900

    各テーブルへの保存記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,13 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :non_purchased_item, only: [:index, :create]
   def index
     @order_form = OrderForm.new
   end
 
   def create
     @order_form = OrderForm.new(order_params)
-    if @order_form.valid?
-      pay_item
-      @order_form.save
+    if  @order_form.save
       redirect_to root_path
     else
       render :index
@@ -19,6 +17,12 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-   params.require(:order_form).permit(:prefecture_id, :city, :house_number, :building_name, :phone_number,:post_code).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+   params.require(:order_form).permit(:prefecture_id, :city, :house_number, :building_name, :phone_number,:post_code).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  def  non_purchased_item
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id 
+  end
+
 end
