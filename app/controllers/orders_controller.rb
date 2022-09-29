47a902394c35c7269fbe1f8462d1326a9414cc23
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-29 14:57:31 +0900

    クレジット決済処理実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,9 @@ class OrdersController < ApplicationController
 
   def create
     @order_form = OrderForm.new(order_params)
-    if  @order_form.save
+    if @order_form.valid?
+      pay_item
+      @order_form.save
       redirect_to root_path
     else
       render :index
@@ -17,7 +19,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-   params.require(:order_form).permit(:prefecture_id, :city, :house_number, :building_name, :phone_number,:post_code).merge(user_id: current_user.id, item_id: params[:item_id])
+   params.require(:order_form).permit(:prefecture_id, :city, :house_number, :building_name, :phone_number,:post_code).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token], 
+      currency: 'jpy'
+    )
   end
 
   def non_purchased_item
