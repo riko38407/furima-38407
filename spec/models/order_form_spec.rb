4603735d79384491d750c8186226d497d4022eeb
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-29 21:05:06 +0900

    Rubocop実行

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe OrderForm, type: :model do
     @user = FactoryBot.create(:user)
     @item = FactoryBot.create(:item)
     @order_form = FactoryBot.build(:order_form, user_id: @user.id, item_id: @item.id)
-    sleep 0.1
   end
 
   describe '配送先情報の保存' do
@@ -104,7 +103,6 @@ RSpec.describe OrderForm, type: :model do
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include('Phone number is invalid')
       end
-
       it '電話番号が9桁以下では購入できない' do
         @order_form.phone_number = 12_345
         @order_form.valid?
