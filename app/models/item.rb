6a54ca7795602baaa542b2a0d288cd0c3a6a6d71
riko38407 <namakemono0711@yahoo.co.jp>
2022-09-30 15:31:33 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,8 +18,8 @@ class Item < ApplicationRecord
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price, numericality: { with: /\A[0-9]+\z/, message: 'should be half-width numbers' }
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                            format: { with: /\A[0-9]+\z/, message: 'should be half-width numbers'}
   end
   with_options numericality: { other_than: 0, message: 'Select' } do
     validates :category_id
